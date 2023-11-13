7757e3cb3ababa654c6496ac39e53fc1e34d7f82
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-11-13 14:15:44 +0100

    initial commit in frontend git repository

---
+++ src/views-forRouter/home.tsx
@@ -0,0 +1,11 @@
+import { Paper } from '@mui/material';
+
+export const Home = () => {
+  return (
+    <Paper>
+      <main className="intro">
+        <h1>Mentoring frontend start</h1>
+      </main>
+    </Paper>
+  );
+};
