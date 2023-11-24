a07ebce241ec111d7b3e71e4a3e47c0ca22bb390
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-11-24 15:03:34 +0100

    added type fixes and other esLint tips

--- src/api/axios/useAxios.types.ts
+++ src/api/axios/useAxios.types.ts
@@ -2,7 +2,7 @@ import type { AxiosError, AxiosRequestConfig } from 'axios';
 
 export interface UseAxiosProps<T> {
   url: string;
-  options?: T | AxiosRequestConfig | any;
+  options?: AxiosRequestConfig;
 }
 
 export interface UseAxiosResult<T> {
