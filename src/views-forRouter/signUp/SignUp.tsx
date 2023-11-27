4c8cf30cdaad3b24a787bb85f1b1f43ca8a5dc49
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-11-27 23:47:30 +0100

    added form for sign up component

--- src/views-forRouter/signUp/SignUp.tsx
+++ src/views-forRouter/signUp/SignUp.tsx
@@ -1,3 +1,29 @@
 export const SignUp = () => {
-  return <h1>Sign Up</h1>;
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <form>
+        <label htmlFor="username">
+          Username
+          <input type="text" id="username" name="username" />
+        </label>
+        <label htmlFor="lastname">
+          Last name
+          <input type="text" id="lastname" name="lastname" />
+        </label>
+        <label htmlFor="email">
+          e-mail
+          <input type="text" id="email" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="text" id="password" name="password" />
+        </label>
+        <label htmlFor="repeatPassword">
+          Repeat password
+          <input type="text" id="repeatPassword" name="repeatPassword" />
+        </label>
+      </form>
+    </>
+  );
 };
