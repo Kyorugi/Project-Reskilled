4fde9a16a2e8b43d8441607ebab0f8d552152182
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-11-29 00:15:40 +0100

    added material elements for input and  validation for incorrect password and firstName

--- src/views-forRouter/signUp/SignUp.tsx
+++ src/views-forRouter/signUp/SignUp.tsx
@@ -1,52 +1,134 @@
 import { Link } from 'react-router-dom';
-import { Box, Button, Paper } from '@mui/material';
+import {
+  Box,
+  Button,
+  Paper,
+  TextField,
+  Input,
+  FormControl,
+  InputLabel,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useState } from 'react';
 
 import { AppRoute } from 'AppRoute';
 
 import * as styles from './SignUp.style';
 
 export const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [firstName, setFirstName] = useState<string>('');
+  const [isFirstNameValid, setIsFirstNameValid] = useState<boolean>(true);
+  const [hasFirstNameBeenTouched, setHasFirstNameBeenTouched] =
+    useState<boolean>(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  const validateName = (name: string): boolean => /^[a-zA-Z]{3,15}$/.test(name);
+  const validateAndSetFirstName = () => {
+    const isValidName = validateName(firstName);
+    setIsFirstNameValid(isValidName);
+  };
+
+  const handleChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+    validateAndSetFirstName();
+    setHasFirstNameBeenTouched(true);
+  };
   return (
     <Box sx={styles.layout}>
       <Paper sx={styles.container}>
         <h1>Sign Up</h1>
-        <form style={{ display: 'flex', flexDirection: 'column' }}>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            placeholder="First Name*"
-          />
+        <TextField
+          onChange={handleChangeFirstName}
+          id="firstName"
+          label="First Name*"
+          variant="standard"
+          error={!isFirstNameValid}
+          helperText={
+            !isFirstNameValid && hasFirstNameBeenTouched
+              ? 'Name should be 3-15 characters long, only letters allowed'
+              : ''
+          }
+        />
 
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            placeholder="Last Name*"
-          />
+        <TextField
+          type="text"
+          id="lastName"
+          label="Last Name*"
+          variant="standard"
+          // helperText="Incorrect entry."
+        />
 
-          <input type="text" id="email" name="email" placeholder="E-mail*" />
+        <TextField
+          type="text"
+          id="email"
+          label="E-mail*"
+          variant="standard"
+          // helperText="Incorrect entry."
+        />
 
-          <input
-            type="text"
-            id="password"
-            name="password"
-            placeholder="Password"
-          />
+        <TextField
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          label="Password*"
+          variant="standard"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          // helperText="Incorrect entry."
+        />
 
-          <input
-            type="text"
-            id="repeatPassword"
-            name="repeatPassword"
-            placeholder="Repeat Password"
-          />
-        </form>
+        <TextField
+          type={showPassword ? 'text' : 'password'}
+          id="repeatPassword"
+          label="Repeat Password*"
+          variant="standard"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          // helperText="Incorrect entry."
+        />
         <Button
           component={Link}
           to={AppRoute.signUp}
           variant="contained"
           size="large"
-          style={{ backgroundColor: '#D44D01', width: 110, height: 35 }}
+          style={{
+            backgroundColor: '#D44D01',
+            width: 110,
+            height: 35,
+            marginTop: 10,
+          }}
         >
           Sign up
         </Button>
